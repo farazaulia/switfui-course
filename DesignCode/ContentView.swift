51ecbc78bea1979aed4c9c7a310cc2f1fd44e432
Farazaulia <raz.muhammad14@gmail.com>
2020-03-20 16:57:31 +0700

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Faraz Muhammad Aulia on 20/03/20.
+//  Copyright © 2020 Faraz Muhammad Aulia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
