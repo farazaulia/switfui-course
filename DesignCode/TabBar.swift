247d0aa986df9747c6ee92205fd04694f464a54a
Farazaulia <raz.muhammad14@gmail.com>
2020-03-21 12:18:24 +0700

    Tab Bar and Test on Device

---
+++ DesignCode/TabBar.swift
@@ -0,0 +1,31 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by Faraz Muhammad Aulia on 21/03/20.
+//  Copyright © 2020 Faraz Muhammad Aulia. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
